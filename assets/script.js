c06e57c4527f055c19d90aea725bb900b9223078
Rizwan Ashraf <riz1ash786@gmail.com>
2022-01-28 17:36:38 +0000

    css sizing and card contents using jQuery

--- assets/script.js
+++ assets/script.js
@@ -19,4 +19,24 @@ function weatherDisplay(city) {
     "&appid=" +
     apiKey +
     "&units=metric";
-    
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    // console.log(response);
+
+    var tempC = response.main.temp;
+    var todayDate = moment().format("MM/DD/YYYY");
+    var weatherIcon = response.weather[0].icon;
+    var weatherURL =
+      "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+
+    // Draws APi Data to weather div container
+    $("#city").html("<h3>" + response.name + " " + todayDate + "</h3>");
+    $("#weatherPicture").attr("src", weatherURL);
+    $("#resultTemp").text("Temperature: " + tempC.toFixed(2) + " °C");
+    $("#resultHum").text("Humidity: " + response.main.humidity + "%");
+    $("#resultWind").text("Wind Speed: " + response.wind.speed + " MPH");
+    retrieveUV(response.coord.lat, response.coord.lon);
+  });
+}
